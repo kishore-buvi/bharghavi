c617850508b75939931d5c3ff44238890e45b5ca
New Username <new.email@example.com>
2025-08-12 17:53:40 +0530

    commit 12-08-2025

--- lib/screens/productList/productService.dart
+++ lib/screens/productList/productService.dart
@@ -2,6 +2,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'dart:io';
+import 'package:flutter/material.dart';
 
 class ProductService {
 final FirebaseFirestore _firestore = FirebaseFirestore.instance;
